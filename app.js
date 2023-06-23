f3669ba3630c396619ce139d9b213244049e4d09
Michelle Hoffmann <michelley9v@yahoo.com>
2023-06-23 11:12:15 -0500

    app additions

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ console.log(
 );
 
 // Exercise 1
-let lastName = "Ismail"
-var age = 58;
+
+let lastName = "Ismail";
+lastName = "Ismail";
+
+var age = 22;
+age = 22;
+
 // Exercise 2
-const firstName = "Ezz"
-firstName = "Ezz"
+const firstName = "Jasmine";
+firstName = "Jasmine"
+
+lastName = "Babagonush";
+ age = "21";
+
+
 
 // Exercise 3
+let language ="Javascript";
+let createYear = 1995;
+let inCaseSensitive = true;
+
+
+console.log (language, createYear,inCaseSensitive);
 
 
 
