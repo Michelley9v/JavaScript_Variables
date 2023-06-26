84922a4f5e89a5afd12ebef3fa1f6bc59b90e501
Michelle Hoffmann <michelley9v@yahoo.com>
2023-06-25 23:01:22 -0500

    JS Veriables 1

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+
 
 // Exercise 1
 
@@ -25,8 +23,33 @@ let language ="Javascript";
 let createYear = 1995;
 let inCaseSensitive = true;
 
+ console.log ('$ {Language} was first released in $ {createYear} $ {inCaseSensitive}.');
 
-console.log (language, createYear,inCaseSensitive);
+let product = "T-Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePersentage = 15;
+let stock = 0;
+let inStock = false;
+let selectSize = "3XL"
+
+console. Log(
+`${product} in size ${selectedSize} is $${price} with ${stock}'
+ in stock.`
+ );
+
+ let title = "Name of the Wind";
+ let author = "patrick Rothfuss";
+ let pageCount = 722;
+ let bookmark = 411;
+ let hasRead = true;
+
+console. Log (
+ `$(title) by $(author} is ${pageCount} pages long. Your last 
+  bookmark was page ${boardders}.`
+);
 
 
+
+  
 
